39fb77af9bbb4833a3d50149b844e5bcf57ec0ce
VineethaPamujula <vineetha9652969476@gmail.com>
2024-10-29 13:14:49 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const fs = require('fs');
 
+// Function to decode the value from a given base to decimal
 function decodeValue(base, value) {
     return parseInt(value, base);
 }
 
-function lagrangeInterpolation(points) {
-    let c = 0;
-
-    for (let i = 0; i < points.length; i++) {
-        let xi = points[i][0];
-        let yi = points[i][1];
-
-        let productTerm = yi;
-        for (let j = 0; j < points.length; j++) {
-            if (i !== j) {
-                let xj = points[j][0];
-                productTerm *= (0 - xj) / (xi - xj);
+// Function to perform Gaussian elimination
+function gaussianElimination(matrix, n) {
+    for (let i = 0; i < n; i++) {
+        // Make the pivot 1
+        for (let j = i + 1; j < n; j++) {
+            const factor = matrix[j][i] / matrix[i][i];
+            for (let k = i; k <= n; k++) {
+                matrix[j][k] -= factor * matrix[i][k];
             }
         }
-        c += productTerm;
     }
-    return Math.round(c);
+
+    // Back substitution
+    const coefficients = Array(n).fill(0);
+    for (let i = n - 1; i >= 0; i--) {
+        coefficients[i] = matrix[i][n] / matrix[i][i];
+        for (let j = i + 1; j < n; j++) {
+            coefficients[i] -= (matrix[i][j] / matrix[i][i]) * coefficients[j];
+        }
+    }
+    return coefficients;
 }
 
 function findConstantTerm(fileName) {
@@ -29,7 +34,6 @@ function findConstantTerm(fileName) {
 
     const n = jsonData.keys.n;
     const k = jsonData.keys.k;
-    const m = k - 1;
 
     const points = [];
     for (const [key, obj] of Object.entries(jsonData)) {
@@ -43,8 +47,22 @@ function findConstantTerm(fileName) {
         throw new Error(`Not enough points to solve the polynomial. Minimum required is ${k}, but got ${points.length}`);
     }
 
-    const selectedPoints = points.slice(0, k);
-    return lagrangeInterpolation(selectedPoints);
+    // Create Vandermonde matrix
+    const matrix = Array(k).fill(0).map(() => Array(k + 1).fill(0));
+    for (let i = 0; i < k; i++) {
+        const x = points[i][0];
+        const y = points[i][1];
+        for (let j = 0; j < k; j++) {
+            matrix[i][j] = Math.pow(x, j); // Fill the Vandermonde matrix
+        }
+        matrix[i][k] = y; // Last column is the y value
+    }
+
+    // Perform Gaussian elimination to solve for coefficients
+    const coefficients = gaussianElimination(matrix, k);
+
+    // Return the constant term (first coefficient)
+    return Math.round(coefficients[0]);
 }
 
 function main() {
